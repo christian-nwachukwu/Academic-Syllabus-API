d7daa01236f5481f259a09211fd13685d0e1aaee
christian nwachukwu <christian.nwachukwu2020@gmail.com>
2022-05-08 16:17:29 +0100

    Database connection

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const dotenv = require('dotenv');
+const express = require('express');
+require('dotenv/config');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//connect to DB
+mongoose.connect(process.env.connectDB, 
+    {useNewUrlParser: true}, ()=>{
+    console.log('connected to DB')
+});
+
 //path to dotenv file
-dotenv.config({path:'config.env'})
 const port = process.env.port || 4000;
 
-app.get('/', (req, res) =>{
-    res.send('index page');
+//Middlewares
+app.use('/post', ()=>{
+    console.log('middleware running')
 });
+
 
 app.listen(port, console.log(`Server listening on http://localhost:${port}...Type Ctrl C to exit`));
